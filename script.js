ab8df11ffd8bfbf5b51a039ff588343ac7b697ba
unknown <arthurmatosversiani@gmail.com>
2025-02-21 10:50:38 -0300

    update

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
+import { armaduras } from "./armaduras.js";
 import { capacetes } from "./capacetes.js";
 import { escudos } from "./escudos.js";
+import { espadas } from "./espadas.js";
+import { machados } from "./machados.js";
 
 
 const tbodyitens = document.getElementById('tbody-itens')
 const btnCapacetes = document.getElementById('btn-capacetes')
 const btnEscudos = document.getElementById('btn-escudos')
+const btnEspadas = document.getElementById('btn-espadas')
+const btnArmaduras = document.getElementById('btn-armaduras')
+const btnMachados = document.getElementById('btn-machados')
 
 capacetes.forEach((element, index) => {
    tbodyitens.innerHTML += `
@@ -12,6 +18,7 @@ capacetes.forEach((element, index) => {
          <td><img src="${element.imagem}" alt="Not found"></td>
          <td>${element.nome}</td>
          <td>${element.defesa}</td>
+         <td> 0 </td>
       </tr>
       `
 })
@@ -21,7 +28,7 @@ capacetes.forEach((element, index) => {
 btnEscudos.addEventListener('click', () => {
    tbodyitens.innerHTML = ""
 
-   escudos.forEach((element,index) => {
+   escudos.forEach((element, index) => {
       tbodyitens.innerHTML += `
       <tr> 
          <td><img src="${element.imagem}" alt="Not found"></td>
@@ -37,13 +44,60 @@ btnEscudos.addEventListener('click', () => {
 btnCapacetes.addEventListener('click', () => {
    tbodyitens.innerHTML = ""
 
-   capacetes.forEach((element,index) => {
+   capacetes.forEach((element, index) => {
       tbodyitens.innerHTML += `
        <tr> 
          <td><img src="${element.imagem}" alt="Not found"></td>
          <td>${element.nome}</td>
          <td>${element.defesa}</td>
+         <td> 0 </td>
+         
       </tr>
       `
    })
+})
+
+btnEspadas.addEventListener('click', () => {
+   tbodyitens.innerHTML = ""
+
+   espadas.forEach((element, index) => {
+      tbodyitens.innerHTML += `
+      <tr> 
+         <td><img src="${element.img}" alt="Not found"></td>
+         <td>${element.nome}</td>
+         <td> 0 </td>
+         <td>${element.ataque}</td>
+      </tr>
+      `
+   })
+})
+
+btnArmaduras.addEventListener('click', () => {
+   tbodyitens.innerHTML = ""
+
+   armaduras.forEach((element, index) => {
+      tbodyitens.innerHTML += `
+      <tr> 
+         <td><img src="${element.imagem}" alt="Not found"></td>
+         <td>${element.nome}</td>
+         <td>${element.defesa}</td>
+         <td> 0 </td>
+      </tr>
+      `
+   })
+})
+
+btnMachados.addEventListener('click', () => {
+tbodyitens.innerHTML = ""
+
+machados.forEach((element, index) => {
+   tbodyitens.innerHTML += `
+   <tr> 
+      <td><img src="${element.img}" alt="Not found"></td>
+      <td>${element.nome}</td>
+      <td> 0 </td>
+      <td>${element.ataque}</td>
+   </tr>
+   `
+})
 })
